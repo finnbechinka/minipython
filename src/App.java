74c9bd7869fd51c82d09581e4fa81b83d57dcadc
Finn <contact@bechinka.eu>
2022-11-26 16:45:59 +0100

    feat: read source code from file

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.util.Collections;
 import Nodes.*;
 import org.antlr.v4.gui.TreeViewer;
@@ -6,8 +9,21 @@ import org.antlr.v4.runtime.tree.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        MiniPythonLexer lexer = null;
+        try(BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
         
-        MiniPythonLexer lexer = new MiniPythonLexer(CharStreams.fromStream(System.in));
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            String everything = sb.toString();
+            lexer = new MiniPythonLexer(CharStreams.fromString(everything));
+        }catch(Exception e){
+            lexer = new MiniPythonLexer(CharStreams.fromStream(System.in));
+        }
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         MiniPythonParser parser = new MiniPythonParser(tokens);
 
