c191f81663f2e21ba264dfd2f5e1ef46cba3ff38
cv7l1 <0xcv7l1@protonmail.com>
2022-11-29 00:56:28 +0100

    add setter methods

--- src/nodes/ClazzDefNode.java
+++ src/nodes/ClazzDefNode.java
@@ -1,23 +1,20 @@
 package nodes;
 
-import java.util.List;
-
 import org.antlr.v4.runtime.tree.Tree;
-
 import scopes.Scope;
 import visitors.ASTVisitor;
 
-public class ClazzDefNode extends ASTNode{
+import java.util.List;
+
+public class ClazzDefNode extends ASTNode {
 
     private Scope scope;
 
     private String id;
     private String parentId;
-
     private List<ASTNode> methods;
 
-    public ClazzDefNode(String id, String parentId, List<ASTNode> methods){
-        
+    public ClazzDefNode(String id, String parentId, List<ASTNode> methods) {
         this.id = id;
         this.parentId = parentId;
         this.methods = methods;
@@ -31,16 +28,28 @@ public class ClazzDefNode extends ASTNode{
         return methods.size();
     }
 
-    public String getId(){
+    public String getId() {
         return this.id;
     }
 
-    public String getParentId(){
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getParentId() {
         return this.parentId;
     }
 
-    public List<ASTNode> getMethods(){
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    public List<ASTNode> getMethods() {
         return this.methods;
+    }
+
+    public void setMethods(List<ASTNode> methods) {
+        this.methods = methods;
     }
 
     @Override
@@ -62,5 +71,4 @@ public class ClazzDefNode extends ASTNode{
     public void setScope(Scope scope) {
         this.scope = scope;
     }
-    
 }
