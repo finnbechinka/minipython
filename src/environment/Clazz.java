fae0a44344ade06a85e5641e96345c9b873aea00
cv7l1 <0xcv7l1@protonmail.com>
2022-11-29 01:00:44 +0100

    implement new methods
    + add inner class 'Instance'
    + findMethod(id): query methods by name

--- src/environment/Clazz.java
+++ src/environment/Clazz.java
@@ -3,25 +3,27 @@ package environment;
 import visitors.ASTEvalVisitor;
 
 import java.util.List;
+import java.util.Map;
 
 public class Clazz extends Callable {
 
     private String name;
     private Environment env;
+    private final Map<String, Function> methods;
 
-    public Clazz(String name, Environment env) {
+    public Clazz(String name, Environment env, Map<String, Function> methods) {
         this.name = name;
         this.env = env;
+        this.methods = methods;
+    }
+
+    public Function findMethod(String id) {
+        return methods.get(id);
     }
 
     @Override
     public void call(ASTEvalVisitor state, List<Object> args) {
-
-    }
-
-    @Override
-    public void ret(Object val) {
-
+        throw new Return(new Instance(this));
     }
 
     public String getName() {
@@ -38,5 +40,24 @@ public class Clazz extends Callable {
 
     public void setEnv(Environment env) {
         this.env = env;
+    }
+
+    public class Instance {
+        private final Clazz clazz;
+
+        public Instance(Clazz clazz) {
+            this.clazz = clazz;
+        }
+
+        public Function get(String name) {
+            Function func = this.clazz.findMethod(name);
+
+            if (func != null) return func.bind(this);
+            else throw new RuntimeException("PANIC");
+        }
+
+        public String toString() {
+            return this.clazz.getName();
+        }
     }
 }
