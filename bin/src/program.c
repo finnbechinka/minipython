fa57cb30499a9cc30d06144dbb01b9dc59bd89a6
Finn <contact@bechinka.eu>
2023-01-01 16:09:03 +0100

    build: first build; not sure if this is correct, will have to talk with my good friends aka my team colleagues :)

---
+++ bin/src/program.c
@@ -0,0 +1,32 @@
+#include <stddef.h>
+
+#include "assert.h"
+#include "mpy_aliases.h"
+#include "mpy_obj.h"
+#include "builtins-setup.h"
+#include "function-args.h"
+#include "literals/tuple.h"
+#include "literals/int.h"
+#include "literals/boolean.h"
+#include "literals/str.h"
+#include "type-hierarchy/object.h"
+#include "type-hierarchy/type.h"
+
+
+
+
+int main() {
+	__mpy_builtins_setup();
+	
+	
+	
+	__mpy_obj_ref_dec(ID: a);
+	ID: a = __mpy_obj_init_int(5);
+	__mpy_obj_ref_inc(ID: a);
+	
+	
+	
+	
+	__mpy_builtins_cleanup();
+	return 0;
+}
