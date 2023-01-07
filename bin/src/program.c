a21072b6c05370cba69d87971c6d57b5c22ca552
Finn <contact@bechinka.eu>
2023-01-07 17:24:41 +0100

    build: test

--- bin/src/program.c
+++ bin/src/program.c
@@ -12,17 +12,23 @@
 #include "type-hierarchy/object.h"
 #include "type-hierarchy/type.h"
 
+__MPyObj *a;
 
 
 
 int main() {
 	__mpy_builtins_setup();
+	a = __mpy_obj_init_object();
+	__mpy_obj_ref_inc(a);
 	
 	
 	
-	while (__mpy_boolean_raw(__mpy_call(__mpy_obj_get_attr(__mpy_obj_init_boolean(true), "__bool__"), __mpy_obj_init_tuple(0), NULL))) {
-		__mpy_obj_ref_dec(__mpy_call(print, __mpy_tuple_assign(0, __mpy_obj_init_str_static("haha"), __mpy_obj_init_tuple(1)), NULL));
-	}
+	__mpy_obj_ref_dec(a);
+	a = __mpy_obj_init_int(2);
+	__mpy_obj_ref_inc(a);
+	__mpy_obj_ref_dec(__mpy_call(print, __mpy_tuple_assign(0, a, __mpy_obj_init_tuple(1)), NULL));
+	
+	__mpy_obj_ref_dec(a);
 	
 	
 	
