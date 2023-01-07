6c02adee39a80a0331c9b03e7b7c2cced917945c
Finn <contact@bechinka.eu>
2023-01-07 17:57:38 +0100

    build: test

--- bin/src/program.c
+++ bin/src/program.c
@@ -12,41 +12,43 @@
 #include "type-hierarchy/object.h"
 #include "type-hierarchy/type.h"
 
-__MPyObj *func1;
-__MPyObj *func_func1(__MPyObj *args, __MPyObj *kwargs)
-{
-	assert(args != NULL && kwargs != NULL);
 
+__MPyObj *func1;
+__MPyObj* func_func1(__MPyObj *args, __MPyObj *kwargs) {
+	assert(args != NULL && kwargs != NULL);
+	
 	__MPyGetArgsState argHelper = __mpy_args_init("func1", args, kwargs, 1);
 	__MPyObj *x = __mpy_args_get_positional(&argHelper, 0, "x");
 	__mpy_args_finish(&argHelper);
-
+	
 	__MPyObj *retValue = NULL;
-
+	
 	__mpy_obj_ref_dec(__mpy_call(print, __mpy_tuple_assign(0, x, __mpy_obj_init_tuple(1)), NULL));
-
+	
 	__mpy_obj_ref_dec(x);
-
+	
 	goto ret;
-ret:
-	if (retValue == NULL)
-	{
+	ret:
+	if (retValue == NULL) {
 		retValue = __mpy_obj_init_object();
 	}
 	return __mpy_obj_return(retValue);
 }
 
-int main()
-{
-	__mpy_builtins_setup();
 
+int main() {
+	__mpy_builtins_setup();
+	
 	func1 = __mpy_obj_init_func(&func_func1);
 	__mpy_obj_ref_inc(func1);
-
+	
+	
 	__mpy_obj_ref_dec(__mpy_call(func1, __mpy_tuple_assign(0, __mpy_obj_init_str_static("wow"), __mpy_obj_init_tuple(1)), NULL));
-
+	
+	
 	__mpy_obj_ref_dec(func1);
-
+	
+	
 	__mpy_builtins_cleanup();
 	return 0;
 }
