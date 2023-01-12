5bbb926ed81ddf81bef6edf080c92cc422c16020
Finn <contact@bechinka.eu>
2023-01-12 17:07:54 +0100

    build: self test

--- bin/src/program.c
+++ bin/src/program.c
@@ -26,34 +26,12 @@ __MPyObj* func_A___init__(__MPyObj *args, __MPyObj *kwargs) {
 	
 	__MPyObj *retValue = NULL;
 	
+	__MPyObj *y = __mpy_obj_init_object();
+	__mpy_obj_ref_inc(y);
 	__mpy_obj_ref_dec(__mpy_call(__mpy_super, __mpy_tuple_assign(0, self, __mpy_obj_init_tuple(1)), NULL));
-	__mpy_obj_ref_dec(__mpy_call(print, __mpy_tuple_assign(0, x, __mpy_obj_init_tuple(1)), NULL));
-	
+	__mpy_obj_set_attr(self, "y", x);
 	__mpy_obj_ref_dec(self);
 	__mpy_obj_ref_dec(x);
-	
-	goto ret;
-	ret:
-	if (retValue == NULL) {
-		retValue = __mpy_obj_init_object();
-	}
-	return __mpy_obj_return(retValue);
-}
-__MPyObj *B;
-__MPyObj* func_B___init__(__MPyObj *args, __MPyObj *kwargs) {
-	assert(args != NULL && kwargs != NULL);
-	
-	__MPyGetArgsState argHelper = __mpy_args_init("__init__", args, kwargs, 2);
-	__MPyObj *self = __mpy_args_get_positional(&argHelper, 0, "self");
-	__MPyObj *y = __mpy_args_get_positional(&argHelper, 1, "y");
-	__mpy_args_finish(&argHelper);
-	
-	__MPyObj *retValue = NULL;
-	
-	__mpy_obj_ref_dec(__mpy_call(__mpy_super, __mpy_tuple_assign(0, self, __mpy_tuple_assign(1, y, __mpy_obj_init_tuple(2))), NULL));
-	
-	__mpy_obj_ref_dec(self);
-	__mpy_obj_ref_dec(y);
 	
 	goto ret;
 	ret:
@@ -78,25 +56,15 @@ int main() {
 		__mpy_obj_set_attr(A, "__init__", __init__);
 		__mpy_obj_ref_dec(__init__);
 	}
-	B = __mpy_obj_init_type("B", A);
-	__mpy_obj_ref_inc(B);
-	{
-		__MPyObj *__init__;
-		__init__ = __mpy_obj_init_func(&func_B___init__);
-		__mpy_obj_ref_inc(__init__);
-		__mpy_obj_set_attr(B, "__init__", __init__);
-		__mpy_obj_ref_dec(__init__);
-	}
 	
 	__mpy_obj_ref_dec(obj);
-	obj = __mpy_call(B, __mpy_tuple_assign(0, __mpy_obj_init_str_static("hi"), __mpy_obj_init_tuple(1)), NULL);
+	obj = __mpy_call(A, __mpy_tuple_assign(0, __mpy_obj_init_int(5), __mpy_obj_init_tuple(1)), NULL);
 	__mpy_obj_ref_inc(obj);
 	
 	__mpy_obj_ref_dec(obj);
 	
 	
 	__mpy_obj_ref_dec(A);
-	__mpy_obj_ref_dec(B);
 	
 	__mpy_builtins_cleanup();
 	return 0;
