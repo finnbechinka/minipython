44b0d8858cf8bed95e87f84465d75fcd4bebbf46
Finn <contact@bechinka.eu>
2023-01-12 18:29:42 +0100

    build: test

--- bin/src/program.c
+++ bin/src/program.c
@@ -14,6 +14,27 @@
 
 __MPyObj *obj;
 
+__MPyObj *moo;
+__MPyObj* func_moo(__MPyObj *args, __MPyObj *kwargs) {
+	assert(args != NULL && kwargs != NULL);
+	
+	__MPyGetArgsState argHelper = __mpy_args_init("moo", args, kwargs, 1);
+	__MPyObj *x = __mpy_args_get_positional(&argHelper, 0, "x");
+	__mpy_args_finish(&argHelper);
+	
+	__MPyObj *retValue = NULL;
+	
+	__mpy_obj_ref_dec(__mpy_call(__mpy_obj_get_attr(x, "foo"), __mpy_obj_init_tuple(0), NULL));
+	
+	__mpy_obj_ref_dec(x);
+	
+	goto ret;
+	ret:
+	if (retValue == NULL) {
+		retValue = __mpy_obj_init_object();
+	}
+	return __mpy_obj_return(retValue);
+}
 
 __MPyObj *A;
 __MPyObj* func_A___init__(__MPyObj *args, __MPyObj *kwargs) {
@@ -62,6 +83,8 @@ int main() {
 	obj = __mpy_obj_init_object();
 	__mpy_obj_ref_inc(obj);
 	
+	moo = __mpy_obj_init_func(&func_moo);
+	__mpy_obj_ref_inc(moo);
 	
 	A = __mpy_obj_init_type("A", __MPyType_Object);
 	__mpy_obj_ref_inc(A);
@@ -84,9 +107,11 @@ int main() {
 	obj = __mpy_call(A, __mpy_obj_init_tuple(0), NULL);
 	__mpy_obj_ref_inc(obj);
 	__mpy_obj_ref_dec(__mpy_call(__mpy_obj_get_attr(obj, "foo"), __mpy_obj_init_tuple(0), NULL));
+	__mpy_obj_ref_dec(__mpy_call(moo, __mpy_tuple_assign(0, obj, __mpy_obj_init_tuple(1)), NULL));
 	
 	__mpy_obj_ref_dec(obj);
 	
+	__mpy_obj_ref_dec(moo);
 	
 	__mpy_obj_ref_dec(A);
 	
