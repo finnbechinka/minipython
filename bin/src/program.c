80b5cf93f1db81b72797dbbe873e8467d9ba7bcc
Finn <contact@bechinka.eu>
2023-01-15 23:42:23 +0100

    build:

--- bin/src/program.c
+++ bin/src/program.c
@@ -14,15 +14,18 @@
 
 __MPyObj *a;
 __MPyObj *b;
+__MPyObj *c;
 
 
 
 int main() {
 	__mpy_builtins_setup();
-	a = __mpy_obj_init_object_w_type("");
+	a = __mpy_obj_init_object_w_type("num");
 	__mpy_obj_ref_inc(a);
-	b = __mpy_obj_init_object_w_type("");
+	b = __mpy_obj_init_object_w_type("num");
 	__mpy_obj_ref_inc(b);
+	c = __mpy_obj_init_object_w_type("bool");
+	__mpy_obj_ref_inc(c);
 	
 	
 	
@@ -31,14 +34,22 @@ int main() {
 	a = __mpy_type_check(a, __mpy_obj_init_int(5));
 	__mpy_obj_ref_inc(b);
 	__mpy_obj_ref_dec(b);
-	b = __mpy_type_check(b, __mpy_obj_init_str_static("str"));
+	b = __mpy_type_check(b, __mpy_obj_init_int(2));
+	__mpy_obj_ref_inc(c);
+	__mpy_obj_ref_dec(c);
+	c = __mpy_type_check(c, __mpy_obj_init_boolean(true));
 	__mpy_obj_ref_inc(a);
 	__mpy_obj_ref_dec(b);
 	b = __mpy_type_check(b, a);
 	__mpy_obj_ref_dec(__mpy_call(print, __mpy_tuple_assign(0, b, __mpy_obj_init_tuple(1)), NULL));
+	__mpy_obj_ref_inc(c);
+	__mpy_obj_ref_dec(b);
+	b = __mpy_type_check(b, c);
+	__mpy_obj_ref_dec(__mpy_call(print, __mpy_tuple_assign(0, b, __mpy_obj_init_tuple(1)), NULL));
 	
 	__mpy_obj_ref_dec(a);
 	__mpy_obj_ref_dec(b);
+	__mpy_obj_ref_dec(c);
 	
 	
 	
