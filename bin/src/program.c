bfc78a869fc85bca90a7d53b07b283f037e1a142
Finn <contact@bechinka.eu>
2023-01-15 17:40:15 +0100

    build:

--- bin/src/program.c
+++ bin/src/program.c
@@ -98,7 +98,7 @@ __MPyObj* func_B_foo(__MPyObj *args, __MPyObj *kwargs) {
 
 int main() {
 	__mpy_builtins_setup();
-	obj = __mpy_obj_init_object_w_type("null");
+	obj = __mpy_obj_init_object_w_type("");
 	__mpy_obj_ref_inc(obj);
 	
 	
