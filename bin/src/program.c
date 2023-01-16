2a1789b40564c9fe01c47995590585c3aa9f088f
Finn <contact@bechinka.eu>
2023-01-16 15:50:59 +0100

    build:

--- bin/src/program.c
+++ bin/src/program.c
@@ -20,10 +20,10 @@ __MPyObj* func_foo(__MPyObj *args, __MPyObj *kwargs) {
 	assert(args != NULL && kwargs != NULL);
 	
 	__MPyGetArgsState argHelper = __mpy_args_init("foo", args, kwargs, 2);
-	__MPyObj *x = __mpy_obj_init_object_w_type("int");
+	__MPyObj *x = __mpy_obj_init_object_w_type("num");
 	__mpy_obj_ref_inc(x);
 	x = __mpy_type_check(x, __mpy_args_get_positional(&argHelper, 0, "x"));
-	__MPyObj *y = __mpy_obj_init_object_w_type("string");
+	__MPyObj *y = __mpy_obj_init_object_w_type("str");
 	__mpy_obj_ref_inc(y);
 	y = __mpy_type_check(y, __mpy_args_get_positional(&argHelper, 1, "y"));
 	__mpy_args_finish(&argHelper);
@@ -31,7 +31,7 @@ __MPyObj* func_foo(__MPyObj *args, __MPyObj *kwargs) {
 	__MPyObj *retValue = NULL;
 	
 	__mpy_obj_ref_dec(__mpy_call(print, __mpy_tuple_assign(0, y, __mpy_obj_init_tuple(1)), NULL));
-	__MPyObj *tmp_ret = __mpy_obj_init_object_w_type("int");
+	__MPyObj *tmp_ret = __mpy_obj_init_object_w_type("num");
 	retValue = __mpy_type_check(tmp_ret, x);
 	goto ret;
 	
@@ -80,7 +80,7 @@ __MPyObj* func_A___init__(__MPyObj *args, __MPyObj *kwargs) {
 	__MPyObj *self = __mpy_obj_init_object_w_type("");
 	__mpy_obj_ref_inc(self);
 	self = __mpy_type_check(self, __mpy_args_get_positional(&argHelper, 0, "self"));
-	__MPyObj *x = __mpy_obj_init_object_w_type("int");
+	__MPyObj *x = __mpy_obj_init_object_w_type("num");
 	__mpy_obj_ref_inc(x);
 	x = __mpy_type_check(x, __mpy_args_get_positional(&argHelper, 1, "x"));
 	__mpy_args_finish(&argHelper);
@@ -129,7 +129,7 @@ __MPyObj* func_A_test(__MPyObj *args, __MPyObj *kwargs) {
 
 int main() {
 	__mpy_builtins_setup();
-	ret = __mpy_obj_init_object_w_type("int");
+	ret = __mpy_obj_init_object_w_type("num");
 	__mpy_obj_ref_inc(ret);
 	obj = __mpy_obj_init_object_w_type("");
 	__mpy_obj_ref_inc(obj);
